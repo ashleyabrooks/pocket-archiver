2fdc135f0dcdcd20e59681d530ee49c5d239d4e0
Ashley Brooks <ashley.brooks.a@gmail.com>
2017-07-05 20:04:10 -0700

    Adding comments

--- archiver.py
+++ archiver.py
@@ -3,6 +3,8 @@ import requests
 def obtain_token():
     """Obtains and returns a request token."""
 
+    # TODO
+
     url = "https://getpocket.com/v3/oauth/request"
     consumer_key = "68610-1a3e291e634b6401538c0fd2"
     redirect_uri = ""
@@ -31,4 +33,6 @@ def get_unread_list():
     url = "https://getpocket.com/v3/get"
     consumer_key = "68610-1a3e291e634b6401538c0fd2"
 
+
+    # TODO 
     pass
